fba0595496ad311ce09bceac1e4b83702b2f6eea
Opossum <reiliallmere@gmail.com>
2021-06-10 11:02:05 +0300

    login and register works

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,12 +51,41 @@
                             <a class="nav-link" href="#menu">Menüü</a>
                         </li>
                         <!-- Authentication Links -->
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">Logi sisse</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">Registreeri</a>
-                        </li>
+                        @guest
+                            @if (Route::has('login'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">Logi sisse</a>
+                                </li>
+                            @endif
+                                @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">Registreeri</a>
+                                </li>
+                                @endif
+                            @else
+                                @if (Auth::user()->admin)
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ url('/admin') }}">{{ __('Admin') }}</a>
+                                    </li>
+                                @endif
+                                    <li class="nav-item dropdown">
+                                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                            {{ Auth::user()->name }}
+                                        </a>
+
+                                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                               onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                                {{ __('Logout') }}
+                                            </a>
+
+                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                                @csrf
+                                            </form>
+                                        </div>
+                                    </li>
+                        @endguest
                         <li class="nav-item">
                             <a class="nav-link" href="#"><i class="bi bi-cart4"></i></a>
                         </li>
