b96e3ab960a90e9580ba7a7afe57f849bc26dd5a
Opossum <reiliallmere@gmail.com>
2021-06-10 21:37:09 +0300

    map, new logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,7 +28,7 @@
         <nav class="navbar navbar-expand-md navbar-custom">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <img src="/../img/logo.svg">
+                    <img id="logo" src="/../img/logo2.svg">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <i class="bi bi-list"></i>
@@ -99,12 +99,30 @@
         <main class="py-4">
             @yield('content')
         </main>
-        <button onclick="topFunction()" id="back-to-top" class="btn" title="Go to top"><i class="bi bi-arrow-up-short"></i></button>
+        <!--<button onclick="topFunction()" id="back-to-top" class="btn" title="Go to top"><i class="bi bi-arrow-up-short"></i></button>-->
+        <div class="modal fade" id="mapModal" tabindex="-1" role="dialog" aria-labelledby="mapModal" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="mapModal">Asukoht</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Sulge">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2028.8788667725178!2d24.74069005114058!3d59.435094809490664!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4692949debbb7cb7%3A0x4695c54b02150724!2sHarju%2040a%2C%2010130%20Tallinn!5e0!3m2!1set!2see!4v1623345798333!5m2!1set!2see" width="100%" height="300px" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Sulge</button>
+                    </div>
+                </div>
+            </div>
+        </div>
         <footer>
             <div class="container">
                 <div class="row py-3">
                     <div class="col-12 col-md-4 text-center text-md-left py-2">
-                        <a href="#" class="footer-font">Harju 40a<br>
+                        <a href="" class="footer-font" data-toggle="modal" data-target="#mapModal">Harju 40a<br>
                             10130 Tallinn<br>
                             Harjumaa, Eesti<br></a>
                         <a href="#" class="footer-font">+372 5555 5555</a><br>
